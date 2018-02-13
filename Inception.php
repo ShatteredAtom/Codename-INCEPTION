cb7ce4b945c16751099cc52128669ac2f9310219
ShatteredAtom <35435955+ShatteredAtom@users.noreply.github.com>
2018-02-12 21:05:49 -0600

    Create Inception.php

---
+++ Inception.php
@@ -0,0 +1,18 @@
+<?php
+script = run = JAVASCRIPT>
+<script = run = PHP>
+<type = 1(var = Simulation:CodeName: INCEPTION>
+public function onDeath(run life count = 0 < x < 125);
+	$object = new = $lifecount(f(x)= 0 < x < 125);
+	…..run $object($lifecount string);
+		if var onDeath = true->
+		-> execute(NATURAL DEATH SCENARIO);
+		-> if life exceeds public var $lifecount execute ->
+			->method (codename: PHILOSIPHERS STONE);
+	} else {
+/*------------
+What if all life started and ended like this………………
+As a line of code on the command line of someone’s Computer…………
+……. What if nothing you know or ever have and or will know or lover will never be or never has been real……… 
+--------*/ 
+run simulation <codename: INCEPTION>
