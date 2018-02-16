24fdb33ebc67aa92629e8610643be20ef00f34a7
ShatteredAtom <35435955+ShatteredAtom@users.noreply.github.com>
2018-02-15 18:05:29 -0600

    Update inception.java

--- inception.java
+++ inception.java
@@ -1,2 +1,11 @@
 <!DOCTYPE = JAVA>
 import java.awt.*;
+import java.net.*;
+import java.human.body.awt.*;
+import java.world.end.event.*;
+public static void main{
+execute order 51 = 39;
+  if true run order("world.end.event");
+} else {
+run death.event("lol");
+}
